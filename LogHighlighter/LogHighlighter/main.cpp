8d79ca13367320742e690c9285455e16a58d4d49
Agustin F. Ruiz <62730262+darksoul96@users.noreply.github.com>
2025-03-26 21:24:53 -0300

    Initial commit

---
+++ LogHighlighter/LogHighlighter/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LogHighlighter
+//
+//  Created by WSO2 on 26/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

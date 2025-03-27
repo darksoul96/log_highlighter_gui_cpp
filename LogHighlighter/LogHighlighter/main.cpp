b60d31c85941620671a14880590a5d731f0e07d4
Agustin F. Ruiz <62730262+darksoul96@users.noreply.github.com>
2025-03-27 02:38:43 -0300

    Silence "deprecated" opengl headers

--- LogHighlighter/LogHighlighter/main.cpp
+++ LogHighlighter/LogHighlighter/main.cpp
@@ -2,6 +2,7 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
+#define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -32,8 +33,7 @@ int main(int, char**)
     ImVec4 clear_color = ImVec4(0.1f, 0.1f, 0.1f, 1.00f);
 
     // Main loop
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
         // Start ImGui frame

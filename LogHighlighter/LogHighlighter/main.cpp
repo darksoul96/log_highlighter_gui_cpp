09c072778f6f55cd5c2b85aaeec5520973555b22
Agustin F. Ruiz <62730262+darksoul96@users.noreply.github.com>
2025-03-27 01:49:15 -0300

    Functional window using GLFW

--- LogHighlighter/LogHighlighter/main.cpp
+++ LogHighlighter/LogHighlighter/main.cpp
@@ -1,14 +1,69 @@
-//
-//  main.cpp
-//  LogHighlighter
-//
-//  Created by WSO2 on 26/03/2025.
-//
+#include "imgui/imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// Main code
+int main(int, char**)
+{
+    if (!glfwInit())
+        return 1;
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "LogHighlighter", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+
+    // Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 120");
+
+    ImVec4 clear_color = ImVec4(0.1f, 0.1f, 0.1f, 1.00f);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // Start ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Simple window
+        ImGui::Begin("LogHighlighter");
+        ImGui::Text("Hello, Log Viewer!");
+        ImGui::End();
+
+        // Render
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
